a641fdc731ffd946adb81735bfb98ec33467c36e
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-12-04 20:26:13 -0800

    remove useless comments

--- server/migration/1733370338756-AddDeletedAtToSession.ts
+++ server/migration/1733370338756-AddDeletedAtToSession.ts
@@ -4,12 +4,10 @@ export class AddDeletedAtToSession1733370338756 implements MigrationInterface {
     name = 'AddDeletedAtToSession1733370338756'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // Add the deletedAt column as nullable
         await queryRunner.query(`ALTER TABLE "session" ADD "deletedAt" datetime NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        // Drop the deletedAt column if we need to roll back
         await queryRunner.query(`ALTER TABLE "session" DROP COLUMN "deletedAt"`);
     }
 }
